f5fc03e4fec4b1088d913a6aa811952db27372e7
Mahendrakumar <Anand.Mahendrakumar@sprint.com>
2019-08-01 20:48:34 -0500

    Adding Files first time

---
+++ app/src/main/java/com/example/gq128783/weatherapp/MainActivity.java
@@ -0,0 +1,194 @@
+package com.example.gq128783.weatherapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView temp,celsius, fahrenheit, cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, weatherIcon, updatedField;
+    ProgressBar loader;
+    Typeface weatherFont;
+    String city = "Milpitas, US";
+    Double a;
+
+
+    // open weather api for data.
+    String OPEN_WEATHER_MAP_API = "token will be here";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+
+        loader = (ProgressBar) findViewById(R.id.loader);
+        ImageView imgFavorite = (ImageView) findViewById(R.id.selectCity);
+        celsius = (TextView) findViewById(R.id.celsius);
+        fahrenheit = (TextView) findViewById(R.id.fahrenheit);
+        cityField = (TextView) findViewById(R.id.city_field);
+        updatedField = (TextView) findViewById(R.id.updated_field);
+        detailsField = (TextView) findViewById(R.id.details_field);
+        currentTemperatureField = (TextView) findViewById(R.id.current_temperature_field);
+        temp = (TextView) findViewById(R.id.temp);
+        humidity_field = (TextView) findViewById(R.id.humidity_field);
+        pressure_field = (TextView) findViewById(R.id.pressure_field);
+        weatherIcon = (TextView) findViewById(R.id.weather_icon);
+        weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weather.ttf");
+        weatherIcon.setTypeface(weatherFont);
+
+        taskLoadUp(city);
+
+        imgFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle("Change City");
+                final EditText input = new EditText(MainActivity.this);
+                input.setHint("City , Country");
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                alertDialog.setView(input);
+
+                alertDialog.setPositiveButton("Change",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                city = input.getText().toString();
+                                taskLoadUp(city);
+                            }
+                        });
+                alertDialog.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+
+
+
+
+        celsius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = Double.parseDouble(String.valueOf(temp.getText()));
+                String r = String.valueOf(a);
+                currentTemperatureField.setText(r);
+                temp.setAlpha(0.0f);
+                celsius.setTextColor(Color.YELLOW);
+                celsius.getBackground();
+                fahrenheit.setTextColor(Color.WHITE);
+
+
+
+            }
+        });
+
+
+        fahrenheit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a=Double.parseDouble(String.valueOf(temp.getText()));
+                Double b=a*9/5+32;
+                String r=String.valueOf(b);
+                currentTemperatureField.setText(r);
+                temp.setAlpha(0.0f);
+                celsius.setTextColor(Color.WHITE);
+                fahrenheit.setTextColor(Color.YELLOW);
+            }
+        });
+
+    }
+
+
+
+    public void taskLoadUp(String query) {
+        if (Function.isNetworkAvailable(getApplicationContext())) {
+            DownloadWeather task = new DownloadWeather();
+            task.execute(query);
+        } else {
+            Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+    class DownloadWeather extends AsyncTask < String, Void, String > {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loader.setVisibility(View.VISIBLE);
+
+        }
+        protected String doInBackground(String...args) {
+            String xml = Function.excuteGet("http://api.openweathermap.org/data/2.5/weather?q=" + args[0] +
+                    "&units=metric&appid=" + OPEN_WEATHER_MAP_API);
+            return xml;
+        }
+        @Override
+        protected void onPostExecute(String xml) {
+
+            try {
+                JSONObject json = new JSONObject(xml);
+                if (json != null) {
+                    JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+                    JSONObject main = json.getJSONObject("main");
+                    DateFormat df = DateFormat.getDateTimeInstance();
+
+                    cityField.setText(json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country"));
+                    detailsField.setText(details.getString("description").toUpperCase(Locale.US));
+                    temp.setText(" " + String.format("%.2f", main.getDouble("temp")) + "");
+                    humidity_field.setText("Humidity: " + main.getString("humidity") + "%");
+                    pressure_field.setText("Pressure: " + main.getString("pressure") + " hPa");
+                    updatedField.setText(df.format(new Date(json.getLong("dt") * 1000)));
+                    weatherIcon.setText(Html.fromHtml(Function.setWeatherIcon(details.getInt("id"),
+                            json.getJSONObject("sys").getLong("sunrise") * 1000,
+                            json.getJSONObject("sys").getLong("sunset") * 1000)));
+
+                    loader.setVisibility(View.GONE);
+
+
+                }
+
+
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Error, Check City", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+}
